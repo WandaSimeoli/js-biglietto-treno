23a728d475e03f16521cfa98f04c8c688126a5b5
WandaSimeoli <wandasimeoli@gmail.com>
2023-06-12 18:28:40 +0200

    Esercizio completato (spero bene)

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
-const km = prompt('Inserisci il numero di km da percorrere'); 
-const age = prompt('Inserisci la tua età');
+const km = parseInt(prompt('Inserisci il numero di km da percorrere')); 
+const age = parseInt(prompt('Inserisci la tua età'));
 let prezzoBiglietto = km * 0.21;
-console.log(km);
-console.log(age);
+console.log(km, typeof km);
+console.log(age, typeof age);
+
+ if(isNaN(age) || isNaN(km)){
+    alert('Il valore inserito non è valido!')
+ } else
 
 if (age <= 18) {
-  x = prezzoBiglietto - ((prezzoBiglietto * 20) / 100);
+  x = prezzoBiglietto - (prezzoBiglietto * 0.2);
+ } else if (age >= 65) {
+    x = prezzoBiglietto - (prezzoBiglietto * 0.4); 
+ } else {
+    x = prezzoBiglietto;
  }
-console.log(x);
+console.log(x, typeof x);
+
+document.querySelector('#my_id').innerHTML = x.toFixed(2)
